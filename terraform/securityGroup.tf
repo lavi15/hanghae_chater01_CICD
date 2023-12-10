9c0a188225c937c6efbd5ee3a3a1c3e8422d3262
lavi15 <biz.jongin@gmail.com>
2023-12-10 15:12:34 +0900

    테라폼 설정 변경

--- terraform/securityGroup.tf
+++ terraform/securityGroup.tf
@@ -15,20 +15,3 @@ resource "aws_security_group" "hanghae_was_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
-
-resource "aws_security_group" "hanghae_db_mysql_sg" {
-  vpc_id = aws_vpc.hanghae_vpc.id
-  name   = "hanghae-db-mysql-sg"
-  ingress {
-    from_port   = 3306
-    to_port     = 3306
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
