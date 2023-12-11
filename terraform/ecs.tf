e44d81e54d287d5c07ee14d7ff191636bc7a87f8
lavi15 <biz.jongin@gmail.com>
2023-12-12 01:59:17 +0900

    초기세팅

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -44,7 +44,7 @@ resource "aws_ecs_service" "hanghae_prod_was" {
   name                               = "hanghae-prod-was"
   cluster                            = aws_ecs_cluster.hanghae_prod_fargate_cluster.id
   task_definition                    = aws_ecs_task_definition.hanghae_prod_was.arn
-  desired_count                      = 2
+  desired_count                      = 1
   deployment_minimum_healthy_percent = 50
   deployment_maximum_percent         = 200
   launch_type                        = "FARGATE"
